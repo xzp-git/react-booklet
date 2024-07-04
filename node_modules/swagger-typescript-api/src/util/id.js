3461c6ca098c40a0e31cadf6ff083b2f4b849e7c
xingzepeng <xingzepeng@bytedance.com>
2024-07-04 17:52:00 +0800

    feat: del modules

--- node_modules/swagger-typescript-api/src/util/id.js
+++
@@ -1,9 +0,0 @@
-const { customAlphabet } = require("nanoid");
-
-const ALPHABET = "abcdefghijklmnopqrstuvwxyz0123456789";
-
-const generateId = customAlphabet(ALPHABET, 12);
-
-module.exports = {
-  generateId,
-};
